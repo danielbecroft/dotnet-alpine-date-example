dc7d632a2e439fbc41bec7a30296aefc985cdb8c
Daniel Becroft <daniel.becroft@superconcepts.com.au>
2023-06-06 00:26:08 +0000

    Downgrade to .NET 6, expand the output, and add the different build images.

--- Program.cs
+++ Program.cs
@@ -10,11 +10,22 @@ namespace dotnet_date_format
         public static void Main(string[] args)
         {
             Console.WriteLine($"CultureInfo.CurrentCulture = {CultureInfo.CurrentCulture}");
+            Console.WriteLine($"CultureInfo.CurrentCulture.CompareInfo.Version.FullVersion = {CultureInfo.CurrentCulture.CompareInfo.Version.FullVersion}");
+            Console.WriteLine($"CultureInfo.CurrentCulture.CompareInfo.Version.SortId = {CultureInfo.CurrentCulture.CompareInfo.Version.SortId}");
+
+            Console.WriteLine($"CultureInfo.InvariantCulture.CompareInfo.Version.FullVersion = {CultureInfo.InvariantCulture.CompareInfo.Version.FullVersion}");
+            Console.WriteLine($"CultureInfo.InvariantCulture.CompareInfo.Version.SortId = {CultureInfo.InvariantCulture.CompareInfo.Version.SortId}");
+            
+            Console.WriteLine($"ICUMode() == {ICUMode()}");
+
             Console.WriteLine($"DateTime.Today = {DateTime.Today}");
+            Console.WriteLine($"DateTime.Today.ToShortDateString() = {DateTime.Today.ToShortDateString()}");
 
             var dtf = CultureInfo.CurrentCulture.DateTimeFormat;
             var dtfi = DateTimeFormatInfo.GetInstance(CultureInfo.CurrentCulture);
 
+            Console.WriteLine($"ShortDatePattern = {dtf.ShortDatePattern}");
+            Console.WriteLine($"FullDateTimePattern = {dtf.FullDateTimePattern}");
             Console.WriteLine($"AbbreviatedMonthNames = {string.Join(",", dtf.AbbreviatedMonthNames)}");
             Console.WriteLine($"AbbreviatedMonthGenitiveNames = {string.Join(",", dtf.AbbreviatedMonthGenitiveNames)}");
 
@@ -30,26 +41,17 @@ namespace dotnet_date_format
             for (int i = 1; i <= 12; i++)
             {
                 var date = new DateTime(2023, i, 1);
-                Console.WriteLine($"date = {date}, dd-MMM-yyyy = {date.ToString("dd-MMM-yyyy")}, dd-MMMM-yyyy = {date.ToString("dd-MMMM-yyyy")}");
+
+                Console.WriteLine($"date = {date}\tMMM = {date.ToString("MMM")}\tdd-MMM-yyyy = {date.ToString("dd-MMM-yyyy")}\tdd-MMMM-yyyy = {date.ToString("dd-MMMM-yyyy")}");
             }
+        }
 
-            foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures).Where(c => c.TwoLetterISOLanguageName == "en"))
-            {
-                var a = culture.DateTimeFormat.AbbreviatedMonthNames;
-                var b = culture.DateTimeFormat.AbbreviatedMonthGenitiveNames;
-
-                if (a.Any(_ => !b.Contains(_)) || b.Any(_ => !a.Contains(_)))
-                {
-                    Console.WriteLine($"culture == {culture}");
-                    Console.WriteLine($"\t AbbreviatedMonthNames => {string.Join(',', a)}");
-                    Console.WriteLine($"\t AbbreviatedMonthGenitiveNames => {string.Join(',', b)}");
-                }
-                else
-                {
-                    Console.WriteLine($"culture == {culture} matches.");
-                }
-
-            }
+        public static bool ICUMode()
+        {
+            SortVersion sortVersion = CultureInfo.InvariantCulture.CompareInfo.Version;
+            byte[] bytes = sortVersion.SortId.ToByteArray();
+            int version = bytes[3] << 24 | bytes[2] << 16 | bytes[1] << 8 | bytes[0];
+            return version != 0 && version == sortVersion.FullVersion;
         }
     }
 }
